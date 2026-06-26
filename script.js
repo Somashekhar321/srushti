058d6ec6f0888acf3df2bd596811cacac1cc191f
Somashekhar321 <130850328+Somashekhar321@users.noreply.github.com>
2026-06-26 21:43:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("btn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", function () {
+    message.textContent = "🎉 Your GitHub Pages website is working!";
+});
